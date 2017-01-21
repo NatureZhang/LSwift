c464c7fe097eed2dcf3c45643fe00e3725a17de9
NatureZhang <895234387@qq.com>
2017-01-21 19:04:12 +0800

    下标语法

--- LSwift/下标.playground/Contents.swift
+++ LSwift/下标.playground/Contents.swift
@@ -1,4 +1,84 @@
 
+//: ### 下标
+
+//: 可以使用下表索引值来设置或者检索值，而不是使用实例方法。可以为一个类型定义多个下标，并且下标会基于传入的索引值的类型选择合适的下标重载使用。下标没有限制单个维度，你可以使用多个输入形参来定义下标以满足自定义类型的需求
+
+//: #### 下标语法
+/*:
+ subscript(index: Int) -> Int {
+    get {
+ 
+    }
+    
+    set(newValue) {
+ 
+    }
+ }
+
+ */
+struct TimesTable {
+    let multiplier: Int
+    subscript(index: Int) -> Int {
+        return multiplier * index
+    }
+}
+
+let threeTimesTable = TimesTable(multiplier: 3)
+print("six times three is \(threeTimesTable[6])")
+
+//: 下标用法
+var numberOfLegs = ["spider": 8, "ant": 6, "cat": 4]
+numberOfLegs["bird"] = 2
+
+//: #### 下标选项
+//: 下标可以接收任意数量的输入形式参数，并且这些输入形式参数可以是任意类型。下标也可以返回任意类型。下标可以使用变量形式参数和可变形式参数，但是不能使用输入输出形式参数或提供默认形式参数值
+
+struct Matrix {
+    let rows: Int, columns: Int
+    var grid: [Double]
+    
+    init(rows: Int, columns: Int) {
+        self.rows = rows
+        self.columns = columns
+        grid = Array(repeating: 0.0, count: rows * columns)
+    }
+    
+    func indexIsValidForRow(row: Int, column: Int) -> Bool {
+        return row >= 0 && row < rows && column >= 0 && column < columns
+    }
+    
+    subscript (row: Int, column: Int) -> Double {
+        get {
+            assert(indexIsValidForRow(row: row, column: column), "index out of range")
+            return grid[(row * columns) + column]
+        }
+        set {
+            assert(indexIsValidForRow(row: row, column: column), "Index out of range")
+            grid[(row * columns) + column] = newValue
+        }
+    }
+}
+
+var matrix = Matrix(rows: 2, columns: 2)
+
+matrix[0, 1] = 1.5
+matrix[1, 0] = 3.2
+
+print(matrix)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
